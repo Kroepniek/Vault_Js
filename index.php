c1133b18d20d95b4e0b78db90a1fac9972928e17
Daniel <mondzielewski123@wp.pl>
2019-01-10 03:53:29 +0100

    Edited: Changing the code completely finished

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,21 +17,25 @@
 	<div id="container">
 		<div id="lock">
 			<div id="Dig_1" onclick="ScrollNum(1)">
-				<div id="D_1_N_0" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">1</div>
-				<div id="D_1_N_1" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">0</div>
-				<div id="D_1_N_2" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">2</div>
+				<div id="D_1_N_0" class="digit">1</div>
+				<div id="D_1_N_1" class="digit">0</div>
+				<div id="D_1_N_2" class="digit">9</div>
 			</div>
 			<div id="Dig_2" onclick="ScrollNum(2)">
-				<div id="D_2_N_0" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">1</div>
-				<div id="D_2_N_1" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">0</div>
-				<div id="D_2_N_2" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">2</div>
+				<div id="D_2_N_0" class="digit">1</div>
+				<div id="D_2_N_1" class="digit">0</div>
+				<div id="D_2_N_2" class="digit">9</div>
 			</div>
 			<div id="Dig_3" onclick="ScrollNum(3)">
-				<div id="D_3_N_0" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">1</div>
-				<div id="D_3_N_1" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">0</div>
-				<div id="D_3_N_2" class="digit" unselectable="on" onselectstart="return false;" onmousedown="return false;">2</div>
+				<div id="D_3_N_0" class="digit">1</div>
+				<div id="D_3_N_1" class="digit">0</div>
+				<div id="D_3_N_2" class="digit">9</div>
 			</div>
 		</div>
+		<form action="" method="post">
+			<button id="Submit" type="submit" value="Unlock"></button>
+		</form>
+		<button id="Submit">Unlock</button>
 	</div>
 </body>
 </html>
